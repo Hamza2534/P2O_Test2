0ccbc490250336b057b487610f0067f841cc5bab
Hamza2534 <136712462+Hamza2534@users.noreply.github.com>
2025-08-20 02:58:33 +0600

    Delete improve_collection_efficiency.m

--- improve_collection_efficiency.m
+++
@@ -1,8 +0,0 @@
-function mods = improve_collection_efficiency(mods, efficiency)
-% IMPROVE_COLLECTION_EFFICIENCY - Set formal collection efficiency
-if efficiency < 0 || efficiency > 1
-    warning('Collection efficiency should be between 0 and 1. Value %.2f may be invalid.', efficiency);
-end
-mods.collection_efficiency = efficiency;
-fprintf('  Setting collection efficiency to %.1f%%\n', efficiency*100);
-end
