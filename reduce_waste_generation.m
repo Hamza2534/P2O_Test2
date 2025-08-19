ad354a13797b199d50114f916ebf225599f08cef
Hamza2534 <136712462+Hamza2534@users.noreply.github.com>
2025-08-20 02:55:07 +0600

    Delete reduce_waste_generation.m

--- reduce_waste_generation.m
+++
@@ -1,8 +0,0 @@
-function mods = reduce_waste_generation(mods, reduction_fraction)
-% REDUCE_WASTE_GENERATION - Reduce waste generation by specified fraction
-if reduction_fraction < 0 || reduction_fraction > 1
-    warning('Reduction fraction should be between 0 and 1. Value %.2f may be invalid.', reduction_fraction);
-end
-mods.waste_reduction = reduction_fraction;
-fprintf('  Setting waste reduction to %.1f%%\n', reduction_fraction*100);
-end
