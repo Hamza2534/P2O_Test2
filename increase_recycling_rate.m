433b4ac17e07788b97b4fdb77899fa7476d302e3
Hamza2534 <136712462+Hamza2534@users.noreply.github.com>
2025-08-20 02:58:16 +0600

    Delete increase_recycling_rate.m

--- increase_recycling_rate.m
+++
@@ -1,8 +0,0 @@
-function mods = increase_recycling_rate(mods, rate)
-% INCREASE_RECYCLING_RATE - Set closed-loop recycling rate
-if rate < 0 || rate > 1
-    warning('Recycling rate should be between 0 and 1. Value %.2f may be invalid.', rate);
-end
-mods.recycling_rate = rate;
-fprintf('  Setting recycling rate to %.1f%%\n', rate*100);
-end
